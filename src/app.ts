01d2af7a6314f20af80e396aa7723c22a3db2961
Md. Sakil Anwar <115878208+sakilanwar12@users.noreply.github.com>
2025-06-22 22:29:53 +0600

    refactor: remove unused notes and user controllers, and clean up app routing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
 import express, { Application } from "express";
-import notesRouter from "./controllers/notes.controller";
-import { userRouter } from "./controllers/user.controller";
 import bookRouter from "./routes/book.routes";
 import borrowRouter from "./routes/borrow.routes";
 
 const app: Application = express();
 app.use(express.json());
 
-app.use("/notes", notesRouter);
-app.use("/user", userRouter);
 app.use("/api/books", bookRouter);
 app.use("/api/borrow", borrowRouter);
 
